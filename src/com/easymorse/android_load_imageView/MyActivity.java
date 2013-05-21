4fb356c092c9e5a3905374a0365d1f5e6e638302
wangjun <wangjun@witmob.com>
2013-05-21 09:27:33 +0800

    测试忽略文件

--- src/com/easymorse/android_load_imageView/MyActivity.java
+++ src/com/easymorse/android_load_imageView/MyActivity.java
@@ -12,6 +12,7 @@ public class MyActivity extends Activity {
      * Called when the activity is first created.
      */
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
