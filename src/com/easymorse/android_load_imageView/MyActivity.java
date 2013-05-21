1e76cba31ac04b026ce9d41d9b32a99a6f9767d6
wangjun <wangjun@witmob.com>
2013-05-21 09:42:12 +0800

    测试

--- src/com/easymorse/android_load_imageView/MyActivity.java
+++ src/com/easymorse/android_load_imageView/MyActivity.java
@@ -12,8 +12,6 @@ public class MyActivity extends Activity {
      * Called when the activity is first created.
      */
 
-
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
